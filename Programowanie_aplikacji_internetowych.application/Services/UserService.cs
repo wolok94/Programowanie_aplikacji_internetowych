723e9d6bea8f14554312aefd984f2a69a2d11948
Kamil <kamil.wolak@interia.eu>
2024-11-28 13:00:32 +0100

    Created function for registering user

---
+++ Programowanie_aplikacji_internetowych.application/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Programowanie_aplikacji_internetowych.domain.Dtos.Users;
+using Programowanie_aplikacji_internetowych.domain.Entities;
+using Programowanie_aplikacji_internetowych.domain.Exceptions;
+using Programowanie_aplikacji_internetowych.domain.Interfaces.Repository;
+using Programowanie_aplikacji_internetowych.domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programowanie_aplikacji_internetowych.application.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository, IPasswordHasher)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task Register(RegisterUserDto registerUserDto)
+    {
+        if (registerUserDto.Password != registerUserDto.ConfirmPassword)
+        {
+            throw new ConfirmationPasswordException("Hasła się nie zgadzają");
+        }
+
+        var user = new User
+        {
+            Password = registerUserDto.Password,
+            Email = registerUserDto.Email,
+            FirstName = registerUserDto.FirstName,
+            LastName = registerUserDto.LastName,
+            Username = registerUserDto.Username
+        };
+    }
+}
