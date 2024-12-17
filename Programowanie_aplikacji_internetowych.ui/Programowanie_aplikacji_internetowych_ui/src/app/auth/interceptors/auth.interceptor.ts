430676255628cc3d37873aad22d4d26cf63ef213
Kamil <kamil.wolak@interia.eu>
2024-12-17 14:28:54 +0100

    Repaired generating new token

--- Programowanie_aplikacji_internetowych.ui/Programowanie_aplikacji_internetowych_ui/src/app/auth/interceptors/auth.interceptor.ts
+++ Programowanie_aplikacji_internetowych.ui/Programowanie_aplikacji_internetowych_ui/src/app/auth/interceptors/auth.interceptor.ts
@@ -6,37 +6,49 @@ import { environment } from '../../../environments/environment.development';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const http = inject(HttpClient);
 
-  const apiUrl = environment.apiUrl + "/token/refresh";
+  const apiUrl = environment.apiUrl + "/User/RefreshToken";
 
-  if (req.url.includes('token/refresh')) {
+  if (req.url.includes('/User/RefreshToken')) {
     return next(req);
   }
 
   if (typeof window !== 'undefined' && localStorage) {
-    const token = localStorage.getItem('accessToken');
+    let token = localStorage.getItem('accessToken');
     const accessTokenExpiresAt = localStorage.getItem('accessTokenExpiresAt');
     const refreshToken = localStorage.getItem('refreshToken');
 
     if (accessTokenExpiresAt) {
-      const expiresAt = new Date(accessTokenExpiresAt).getTime();
+      const [dateSplit, timeSplit] = accessTokenExpiresAt.split(" ");
+      const [hours, minutes, seconds] = timeSplit.split(":");
+      const [day, month, year] = dateSplit.split(".");
+      const isoString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+
+      const timestamp = new Date(isoString).getTime();
+      const expiresAt = new Date(timestamp).getTime();
       const now = new Date().getTime();
 
       if (now > expiresAt) {
 
-        if (refreshToken) {
-          return http.post(apiUrl, null, { headers: req.headers.set("Refresh-Token" , refreshToken),
+        if (refreshToken && token) {
+          return http.post(apiUrl, null, {
+            headers: req.headers.set("Refresh-Token", refreshToken)
+              .set("Access-Token", token),
             observe: 'response'
           }).pipe(
             switchMap((response: any) => {
-              const newAccessToken = response.headers.get('Access-Token');
-              const newAccessTokenExpiresAt = response.headers.get('Access-Token-Expires-At');
+              const newAccessToken = response.headers.get("x-access-token");
+              const newRefreshToken = response.headers.get("x-refresh-token");
+              const newAccessTokenExpiresAt = response.headers.get("x-access-token-expiresat");
+              const newRefreshTokenExpiresAt = response.headers.get("x-refresh-token-expiresat");
 
               localStorage.setItem('accessToken', newAccessToken);
-              localStorage.setItem('accessTokenExpiresAt', newAccessTokenExpiresAt);
-
+              localStorage.setItem('refreshToken', newRefreshToken);
+              localStorage.setItem('accessTokenExpiresAt', newAccessTokenExpiresAt.toString());
+              localStorage.setItem('refreshTokenExpiresAt', newRefreshTokenExpiresAt.toString());
+              token = newAccessToken;
 
               return next(req.clone({
-                headers: req.headers.set("Access-Token", newAccessToken)
+                headers: req.headers.set("Authorization", "Bearer " + newAccessToken)
               }));
             })
           );
